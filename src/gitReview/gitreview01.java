ba6864cbf46772ebc66a8b5ac2c41ccea0ef8ecf
Your Name <you@example.comclear>
2022-05-31 21:52:27 -0400

    2nd commit

--- src/gitReview/gitreview01.java
+++ src/gitReview/gitreview01.java
@@ -4,7 +4,9 @@ public class gitreview01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("today we learn github");
+		System.out.println("today is a very good day");
+		System.out.println("today is a very good day");
 	}
 
 }
